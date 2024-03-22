7d982e14a85ba1d7ae834fb4a8a673f8c7ebf2f5
MD. AL RASEL <mdalrasel96@gmail.com>
2024-03-22 10:17:06 +0600

    menu toggol option

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,7 +1,12 @@
+import { useState } from "react";
 import Link from "../Link/Link";
+import { HiMenuAlt2} from "react-icons/hi";
+import { FaRegWindowClose } from "react-icons/fa";
+
 
 
 const NavBar = () => {
+    const [open, setOpen] =useState(false)
     const routes = [
         {id:1,name:'Home',path:'/'},
         {id:2,name:'About',path:'/about'},
@@ -12,6 +17,12 @@ const NavBar = () => {
 
     return (
         <nav>
+            <div onClick={() => setOpen(!open)} className="text-2xl md:hidden">
+            {
+                open === true ?  <FaRegWindowClose /> :<HiMenuAlt2 />
+            }
+                
+            </div>
            <ul className="md:flex ">
            {
                 routes.map(route => <Link key={route.id} route={route}></Link>)
